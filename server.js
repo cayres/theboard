1ec74e7e7f96eb4539986970db85992e36f5a97d
cayres <rcayres18@gmail.com>
2016-05-01 04:02:22 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+/// <reference path="typings/tsd.d.ts" />
+
+var http = require("http");
+var express = require("express");
+var app = express();
+var controllers = require("./controllers");
+// var ejsEngine = require("ejs-locals");
+
+//Setup View Engine
+// app.set("view engine", "jade");
+// app.engine("ejs", ejsEngine); // support master page
+// app.set("view engine", "ejs"); // ejs view engine
+app.set("view engine", "vash");
+
+//Map the route
+controllers.init(app);
+
+// app.get("/", function (req, res) {
+//     // res.send("<html><body><h1>Express</h1></body></html>");
+//     // res.render("jade/index", {title: "Express + Jade"});
+//     // res.render("ejs/index", {title: "Express + EJS"});
+//     res.render("index", {title: "Express + Vash"});
+// })
+
+var server = http.createServer(app);
+
+server.listen(3000)
