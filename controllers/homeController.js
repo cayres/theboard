0dc5eaa79382b048f56ee517384893d63cce02af
cayres <rcayres18@gmail.com>
2016-05-02 06:29:47 -0300

    data service

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,19 @@
 (function (homeController) {
     
+    var data = require("../data")
+    
     homeController.init = function (app) {
         
-        app.get("/", function (req, res) {    
-            res.render("index", {title: "Express + Vash"});
+        app.get("/", function (req, res) {
+            
+            data.getNoteCategories(function (err, results) {
+                res.render("index", {
+                    title: "Express + Vash", 
+                    error: err, 
+                    categories: results
+                });
+            });               
+            
         });
     };
 })(module.exports)
